88159121fdefdc3f949fca0d80905e86e76b6a13
Mariasiu Vlad <vlad.mariasiu@around25.com>
2019-01-04 13:24:41 +0200

    enable cors

--- src/http/index.js
+++ src/http/index.js
@@ -3,6 +3,12 @@ const { http: config } = require('../config');
 
 const server = express();
 server.get('/ping', (req, res) => res.json({ success: true }));
+// enable cors
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 server.listen(config.port, () => console.log(`Started http server on port ${config.port}!`));
 
 module.exports = server;
